db52739161690ea629aef5a6ef3455f82d998fb5
franckniat <niatmanuel2002@gmail.com>
2025-05-06 02:46:53 +0100

    update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -7,20 +7,20 @@ export default function Home() {
 		<div className="flex items-center justify-center min-h-screen gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="max-w-[800px] mx-auto px-3">
 				<div className="flex items-center flex-col gap-4">
-					<span className="text-3xl sm:text-5xl">📸</span>
-					<h1 className="text-4xl font-extrabold text-center">
-						Welcome to the web camera app
+					<span className="text-3xl sm:text-5xl lg:text-6xl">📸</span>
+					<h1 className="text-3xl md:text-4xl lg:text-6xl font-extrabold text-center">
+						Welcome to the <span className="text-primary underline">web camera</span> app
 					</h1>
-					<p className="text-lg sm:text-xl mt-5 text-center max-w-3xl">
+					<p className="text-lg sm:text-xl md:text-2xl mt-5 text-center max-w-3xl">
 						This is a simple web camera app that allows you to take
 						pictures and record videos.
 					</p>
 					<div className="flex sm:flex-row flex-col items-center justify-center gap-5 my-10 w-full">
 						<Link href={"/photos"} className="w-full sm:w-fit">
-							<Button size={"lg"} className="w-full sm:w-fit">Get Started</Button>
+							<Button size={"xl"} className="w-full sm:w-fit">Get Started</Button>
 						</Link>
 						<Link href={"https://github.com/franckniat/camera-app"} target="_blank" className="w-full sm:w-fit">
-							<Button size={"lg"} variant={"secondary"} className="gap-2 w-full sm:w-fit"><Github/> Contribute</Button>
+							<Button size={"xl"} variant={"secondary"} className="gap-2 w-full sm:w-fit"><Github/> Contribute</Button>
 						</Link>
 					</div>
 					<div className="absolute bottom-0 left-1/2 -translate-x-1/2 p-5">
